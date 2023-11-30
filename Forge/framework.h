c0bb4f54d0a581ff0411a9a54ca360b3e1b3e0ce
Project-Z1K <149071205+Project-Z1K@users.noreply.github.com>
2023-11-30 20:28:35 +0000

    fix bInfiniteMaterials and bInfiniteAmmo

--- Forge/framework.h
+++ Forge/framework.h
@@ -416,6 +416,8 @@ namespace Globals
 	static inline bool bPlayground = false;
 	static inline bool bRestarting = false;
 	static inline bool bNoMCP = false;
+	static inline bool bInfiniteMaterials = false;
+	static inline bool bInfiniteAmmo = false;
 	static int AmountOfRestarts = 0;
 }
 
